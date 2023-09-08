3897a2ed810c18644c692322da3498b7aee87527
Laba Subagia <labasubagia22@gmail.com>
2023-09-09 07:34:12 +0800

    fix: remove unnecessary

--- internal/core/port/repository.go
+++ internal/core/port/repository.go
@@ -2,13 +2,6 @@ package port
 
 import (
 	"context"
-	"errors"
-)
-
-var (
-	ErrIsolation  = errors.New("Error Isolation")
-	ErrUniqueKey  = errors.New("Error Unique Key")
-	ErrForeignKey = errors.New("Error Foreign Key")
 )
 
 type RepositoryAtomicCallback func(r Repository) error
